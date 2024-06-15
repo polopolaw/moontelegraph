2a6f32675f2acd0fe86a3484b3c2b0bbdc16e6e7
polopolaw <polopolaw@gmail.com>
2024-06-15 11:39:30 +0600

    Add auto register webhook to test bots

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,19 +12,19 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        TelegraphBotFactory::new()
+        $bot = TelegraphBotFactory::new()
             ->has(UserFactory::new()->count(20))
             ->create([
                 'token' => '338570805:AAHBlr7mVxZxBmQG6xbR_Iur7ptsoneU5JE',
                 'name' => '@ssubot',
-            ]);
+            ])->registerWebhook()->send();
 
         TelegraphBotFactory::new()
             ->has(UserFactory::new()->count(20))
             ->create([
                 'token' => '6647318716:AAG9F7dT9ZT_Jea_IoFm1-srXpek0ASXYYc',
                 'name' => '@pora_do_excercise_bot',
-            ]);
+            ])->registerWebhook()->send();
 
         MessageFactory::new()
             ->count(1000)
